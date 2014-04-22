602ff4a43b9ca4af04e4cade24852191abf8fddd
Andrew Berls <andrew.berls@gmail.com>
2014-04-22 13:31:30 -0700

    Code cleanup

--- shell.c
+++ shell.c
@@ -4,20 +4,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-
-const char* exitString = "exit";
-const char* ampString  = "&";
-
-// Basic commands working (i.e. `ls`, `pwd`)
-// Command line arguments working (i.e. `ls -l`)
-// Exiting the shell working (i.e. command `exit`)
+#include <sys/wait.h>
 
 // TODO: Input/Output Redirection
 // TODO: Background Processes
 // TODO: Whitespace Characters
 // TODO: Error Handling (malformed string inputs)
 
-void exitShell(char *string) { if (strcmp(string, exitString) == 0) { exit(1); }}
 
 int len(char** array) {
     int length = 0;
@@ -30,12 +23,13 @@ int len(char** array) {
     return length;
 }
 
+
 char** splitString(char* string, const char charDelim) {
     char delim[2];
-    char* tmp        = string;
-    char* lastChar   = 0;
-    char** result    = 0;
-    size_t count     = 0;
+    char* tmp      = string;
+    char* lastChar = 0;
+    char** result  = 0;
+    size_t count   = 0;
 
     delim[0] = charDelim;
     delim[1] = 0;
@@ -49,10 +43,10 @@ char** splitString(char* string, const char charDelim) {
         tmp++;
     }
 
-    count += lastChar < (string + strlen(string) - 1); // Add space for trailing token
+    // Add space for trailing token
+    count += lastChar < (string + strlen(string) - 1);
 
-    /* Add space for terminating null string so caller
-       knows where the list of returned strings ends. */
+    // Add space for null terminator
     count++;
 
     result = malloc(sizeof(char*) * count);
@@ -74,63 +68,56 @@ char** splitString(char* string, const char charDelim) {
     return result;
 }
 
-// execpv() can call relative to your position
-void execute(char* array[]) {
-    int i = 0;
-    int length;
+
+void handle_command(char* args[]) {
     int status;
-    pid_t pid;
+    pid_t pid = fork();
 
-    length = len(array);
-
-    if (strcmp(array[length - 1], ampString) != 0 ) { // No background process for this command
-        if ((pid = fork()) < 0) { // Failed forked process
-            printf("ERROR: failed to fork child process\n");
+    if (pid < 0) {
+        printf("ERROR: fork() failed\n");
+        exit(1);
+    } else if (pid == 0) {
+        // Child
+        if (execvp(args[0], args) < 0) {
+            printf("ERROR: execvp() failed\n");
             exit(1);
-        } else if (pid == 0) {  // For the child process
-            if (execvp(array[0], array) < 0) { // Execute the command
-                printf("ERROR: exec failed\n");
-                exit(1);
-            }
-        } else {                         // For the parent process
-            while (wait(&status) != pid) // Wait for completion
-                ;
         }
-
-    } else { // TODO: This case handles the background process waiting to happen (i.e. &)
-        /*
-        if ((pid = fork()) == 0) {
-
-        } else {
-
+    } else {
+        // Parent
+        while (wait(&status) != pid) {
+            // Wait for child to finish
         }
-        */
-
-        printf("You are in the else part\n");
     }
 }
 
+
+void handle_bg_command() {
+    printf("TODO: background process\n");
+}
+
+
+void execute(char* args[]) {
+    if (strcmp(args[0], "exit") == 0) { exit(1); }
+
+    int argLen = len(args);
+    if (strcmp(args[argLen-1], "&") != 0) {
+        handle_command(args);
+    } else {
+        handle_bg_command();
+    }
+}
+
+
 int main(int argc, char* argv[]) {
-    //int i;
     char inputLine[1024];
     char** tokens;
 
     while (1) {
         printf("sish:> ");
         gets(inputLine);
-        printf("\n");
 
         tokens = splitString(inputLine, ' ');
-
-        //for (i = 0; *(tokens + i); i++) {
-        //    printf("%s\n", tokens[0]);
-        //    printf("%s\n", *(tokens + i));
-        //    free(*(tokens + i));
-        //}
-
-        exitShell(tokens[0]);
         execute(tokens);
-        //break; // Only run the shell once if not commented out
     }
 
     return 0;
