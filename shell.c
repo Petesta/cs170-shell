3b67bfc6db4b78d9828cffc40ece69d8f92bd183
Petesta <Petesta@live.com>
2014-04-22 14:53:45 -0700

    Background processes are working right now with hard coded values. Just need to make sure input string to dropAmpersand does not mutate when returning a new string.

--- shell.c
+++ shell.c
@@ -1,22 +1,23 @@
 #include <assert.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 
-#define MAX_INPUT_SIZE 1024
+const char* ampString  = "&";
+const char* emptyString = "";
+const char* exitString = "exit";
+
+// Basic commands working (i.e. `ls`, `pwd`)
+// Command line arguments working (i.e. `ls -l`)
+// Exiting the shell working (i.e. command `exit`)
 
 // TODO: Input/Output Redirection
 // TODO: Background Processes
 // TODO: Whitespace Characters
 // TODO: Error Handling (malformed string inputs)
 
-// TODO: dup2/dup(), pipe() and close()
-
-
+void exitShell(char *string) { if (strcmp(string, exitString) == 0) { exit(1); }}
 
 int len(char** array) {
     int length = 0;
@@ -29,14 +30,33 @@ int len(char** array) {
     return length;
 }
 
+// TODO: string is being mutated, we don't want that to happen
+char** dropAmpersand(char** string) {
+    int i = 0;
+    char delim[2];
+    char** newString;
+    char** copy;
 
-// TODO: handle runs of whitespace, ex: "one     two"
+    delim[0] = ' ';
+    delim[1] = 0;
+
+    while (i < len(string) - 1) {
+        //char* token = strtok(string, delim);
+        char* token = strtok(string[i], delim);
+        //char* token = strtok(string, delim);
+        //*(result + idx++) = strdup(token);
+        *(newString + i++) = strdup(token);
+    }
+
+    return newString;
+}
+
 char** splitString(char* string, const char charDelim) {
     char delim[2];
-    char* tmp      = string;
-    char* lastChar = 0;
-    char** result  = 0;
-    size_t count   = 0;
+    char* tmp        = string;
+    char* lastChar   = 0;
+    char** result    = 0;
+    size_t count     = 0;
 
     delim[0] = charDelim;
     delim[1] = 0;
@@ -50,16 +70,20 @@ char** splitString(char* string, const char charDelim) {
         tmp++;
     }
 
-    // Add space for trailing token
-    // TODO: wtf is this
-    count += lastChar < (string + strlen(string) - 1);
+    count += lastChar < (string + strlen(string) - 1); // Add space for trailing token
+    printf("string = %s\n", string);
+    printf("lastChar = %s\n", lastChar);
+    printf("tmp = %s\n", tmp);
+    printf("count = %d\n", count);
+    //exit(1);
 
-    // Add space for null terminator
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
     count++;
 
     result = malloc(sizeof(char*) * count);
 
-    if (result != NULL) {
+    if (result) {
         char* token = strtok(string, delim);
         size_t idx  = 0;
 
@@ -76,104 +100,77 @@ char** splitString(char* string, const char charDelim) {
     return result;
 }
 
-
-void handle_command(char* args[]) {
+// execpv() can call relative to your position
+void execute(char* array[]) {
+    int i = 0;
+    int length;
     int status;
-    pid_t pid = fork();
+    pid_t pid;
 
-    if (pid < 0) {
-        printf("ERROR: fork() failed\n");
-        exit(1);
-    } else if (pid == 0) {
-        // Child
+    length = len(array);
 
-        if (execvp(args[0], args) < 0) {
-            printf("ERROR: execvp() failed\n");
+    //printf("inside execute: %s\n", *dropAmpersand(array));
+
+    if (strcmp(array[length - 1], ampString) != 0 ) { // No background process for this command
+        if ((pid = fork()) < 0) { // Failed forked process
+            printf("ERROR: failed to fork child process\n");
             exit(1);
+        } else if (pid == 0) {  // For the child process
+            if (execvp(array[0], array) < 0) { // Execute the command
+                printf("ERROR: exec failed\n");
+                exit(1);
+            }
+        } else {                         // For the parent process
+            while (wait(&status) != pid) // Wait for completion
+                ;
         }
-    } else {
-        // Parent
-        while (wait(&status) != pid) {
-            // Wait for child to finish
+
+    } else { // TODO: This case handles the background process waiting to happen (i.e. &)
+        printf("You are in the else part\n");
+
+        if ((pid = fork()) == 0) { // Child process
+            //setpgid(0, 0);
+            printf("This is the child process\n");
+            char* temp[] = {"ls"};
+            //char** droppedAmp = dropAmpersand(array);
+            if (execvp(array[0], temp) < 0) {
+                printf("execvp failed\n");
+            } else {
+                printf("execvp succeeded\n");
+            }
+            printf("after conditional *****\n");
+        } else { // Parent process 
+            printf("This is the parent process\n");
+            return;
         }
     }
 }
-
-
-void handle_bg_command() {
-    printf("TODO: background process\n");
-}
-
-
-void execute(char* args[]) {
-    if (strcmp(args[0], "exit") == 0) { exit(1); }
-
-    int argLen = len(args);
-    int i;
-
-
-
-    // TODO: input redirection
-
-
-
-
-    // Check if output is being redirected; if so,
-    // set our stdout to the file descriptor of the specified
-    // output file
-    char* outputFilename = NULL;
-    for (i=0; i<argLen; i++) {
-        if (strcmp(args[i], ">") == 0) {
-            // TODO: slice > and output file off args so they don't get passed to execvp
-            outputFilename = args[i+1];
-            break;
-        }
-    }
-
-    if (outputFilename != NULL) {
-        printf("debug: got output file %s\n", outputFilename);
-
-        FILE* outputFile = fopen(outputFilename, "w+");
-        if (outputFile == NULL) {
-            printf("ERROR: failed to open output file\n");
-            exit(1);
-        }
-
-        if (dup2(fileno(outputFile), STDOUT_FILENO) < 0) {
-          printf("ERROR: in dup2()\n");
-          exit(1);
-        }
-    }
-
-
-
-    if (strcmp(args[argLen-1], "&") != 0) {
-        handle_command(args);
-    } else {
-        handle_bg_command();
-    }
-}
-
-
-
 
 int main(int argc, char* argv[]) {
-    char inputLine[MAX_INPUT_SIZE];
+    //int i;
+    char inputLine[1024];
     char** tokens;
 
     while (1) {
-        if (isatty(STDIN_FILENO)) {
-            printf("sish:> ");
+        printf("sish:> ");
+        gets(inputLine);
+        printf("\n");
+
+        if (strcmp(inputLine, emptyString) != 0) {
+            tokens = splitString(inputLine, ' ');
+            //test = dropAmpersand(tokens);
+            exitShell(tokens[0]);
+            execute(tokens);
         }
 
-        fgets(inputLine, MAX_INPUT_SIZE, stdin);
-        strtok(inputLine, "\n"); // Weird way to remove newline from fgets
+        //for (i = 0; *(tokens + i); i++) {
+        //    printf("%s\n", tokens[0]);
+        //    printf("%s\n", *(tokens + i));
+        //    free(*(tokens + i));
+        //}
+        //
 
-        tokens = splitString(inputLine, ' ');
-        execute(tokens);
-
-        // TODO: exit if not TTY ?
-        if (!isatty(STDIN_FILENO)) { exit(1); }
+        //break; // Only run the shell once if not commented out
     }
 
     return 0;
